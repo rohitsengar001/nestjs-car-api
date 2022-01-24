5b1d8b931dd8068e02e1d8c0067ee72c98790e0a
rohitsengar001 <rohitsengar001@gmail.com>
2022-01-24 16:43:10 +0530

    create shema and interface file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppService } from './app.service';
 import { CarModule } from './car/car.module';
 
 @Module({
-  imports: [CarModule],
+  imports: [
+    CarModule,
+    MongooseModule.forRoot('mongodb://localhost:car_manager')
+  ],
   controllers: [],
   providers: [AppService],
 })
