4e98afe3626dc81d697989c0e63033703a7820ee
rohitsengar001 <rohitsengar001@gmail.com>
2022-01-24 16:05:30 +0530

    define api end point

--- src/car/car.controller.ts
+++ src/car/car.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { CarDto } from './car.dto';
+import { CarService } from './car.service';
+
 
 @Controller('car')
-export class CarController {}
+export class CarController {
+  constructor(private carService: CarService) {}
+
+  @Get()
+  public getCars(){
+      return this.carService.getCars();
+  }
+
+  @Post()
+  public postcar(@Body() car: CarDto){
+    return this.carService.postCars(car);
+  }
+
+  @Get(':id')
+  public getCarById(@Param('id') id:number){
+    return this.carService.getCarById(id);
+  }
+
+  @Delete(':id')
+  public deleteCarById(@Param('id') id:number){
+    return this.carService.deleteCarByID(id);
+  }
+
+  @Put(':id')
+  public put(@Param('id') id,@Query() query) {
+    const propertyName=query.property_name;
+    const propertyValue=query.property_value;
+    return this.carService.putCarById(id,propertyName,propertyValue);
+  }
+}
+
