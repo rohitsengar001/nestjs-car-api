81ce7e16f62742f352688f33edfc06e013c769be
rohitsengar001 <rohitsengar001@gmail.com>
2022-01-24 21:23:12 +0530

    import mangodb module and carschema

--- src/car/car.module.ts
+++ src/car/car.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CarController } from './car.controller';
+import { CarSchema } from './car.schema';
 import { CarService } from './car.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'Car',
+        schema: CarSchema,
+      },
+    ]),
+  ],
   controllers: [CarController],
   providers: [CarService],
 })
